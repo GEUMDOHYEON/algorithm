6d6db2ea6b3ecafa022f14a76eeca562572ff444
GEUMDOHYEON <dohyeon0518@gmail.com>
2024-04-21 14:35:02 +0900

    10026, 9465

--- test.py
+++ test.py
@@ -1,11 +1,6 @@
-N = int(input())
-A = list(map(int,input().split()))
-B = list(map(int,input().split()))
-result = 0
-
-A.sort()
-B.sort(reverse=True)
-print
-for i in range(N):
-    result += (A[i] * B[i])
-print(result)
+print("(___)")
+print("(o o)____/")
+print(" @@      \\")
+print("  \\ ____,/")
+print("  //   //")
+print(" ^^   ^^")
