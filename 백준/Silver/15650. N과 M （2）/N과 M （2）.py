3b02af1fd5f6938fa8cc9b3a847a9e8cb47bcd27
GEUMDOHYEON <154233397+GEUMDOHYEON@users.noreply.github.com>
2024-04-22 10:31:59 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,17 @@
+import sys
+
+N,M = map(int,sys.stdin.readline().split())
+result = []
+
+def dfs(start):
+    if len(result) == M:
+        print((" ").join(map(str,result)))
+        return
+
+    for i in range(start, N + 1):
+        if not i in result:
+            result.append(i)
+            dfs(i)
+            result.pop()
+
+dfs(1)
