cdb2ba59937792c368c0030cfab95315a0fa001b
GEUMDOHYEON <154233397+GEUMDOHYEON@users.noreply.github.com>
2024-06-12 00:09:50 +0900

    [Bronze II] Title: 소수, Time: 4756 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,18 @@
+M = int(input())
+N = int(input())
+
+arr = []
+
+for i in range(M, N+1):
+  count = 0
+  for j in range(2, i):
+    if i % j == 0:
+      count += 1
+  if count == 0 and i > 1:
+    arr.append(i)
+
+if arr:
+  print(sum(arr))
+  print(min(arr))
+else:
+  print(-1)
