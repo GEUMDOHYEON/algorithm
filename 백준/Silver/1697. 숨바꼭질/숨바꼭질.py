e7fce2ed757bda5353a694ba7aad96953725f56e
GEUMDOHYEON <154233397+GEUMDOHYEON@users.noreply.github.com>
2024-06-21 23:02:37 +0900

    [Silver I] Title: 숨바꼭질, Time: 96 ms, Memory: 35288 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,23 +1,22 @@
 import sys
 from collections import deque
 
-read = sys.stdin.readline
+def bfs(n):
+  q = deque()
+  q.append(n)
 
-def dfs(n):
-    q = deque([n])
+  while q:
+    a = q.popleft()
+    if a == K:
+      print(time[a])
+      break
+    for i in (a-1,a+1,a*2):
+      if 0<= i < 100001 and not time[i]:
+        time[i] = time[a] + 1
+        q.append(i)
 
-    while q:
-        tmp = q.popleft()
-        if tmp == K:
-            print(graph[tmp])
-            break
-        for i in (tmp-1,tmp+1,tmp*2):
-            if 0 <= i <= MAX and not graph[i]:
-                graph[i] = graph[tmp] + 1
-                q.append(i)
-                
-N, K = map(int,read().split())
-MAX = 10**5
-graph = [0] * (MAX + 1)
 
-dfs(N)
+N,K = map(int,sys.stdin.readline().split())
+time = [0] * 100001
+
+bfs(N)
