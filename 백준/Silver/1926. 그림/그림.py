e7631ab6810d6356433866dc0188ea9a2a0cf2af
GEUMDOHYEON <154233397+GEUMDOHYEON@users.noreply.github.com>
2025-01-18 14:16:32 +0900

    [Silver I] Title: 그림, Time: 276 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,37 +1,40 @@
 import sys
 from collections import deque
-read = sys.stdin.readline
 
-N, M = map(int,read().split())
+def bfs(a,b):
+  tmp = 1
+  q = deque()
+  q.append((a,b))
+  graph[a][b] = 0
 
-matrix = [list(map(int,read().split())) for _ in range(N)]
+  while q:
+    y, x = q.popleft()
 
-def bfs(x,y):
-    matrix[x][y] = 0
-    dx = [1,-1,0,0]
-    dy = [0,0,1,-1]
-    w = 1
-    q = deque()
-    q.append([x,y])
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if (0<= nx < N) and (0 <= ny < M) and matrix[nx][ny]:
-                q.append([nx,ny])
-                matrix[nx][ny] = 0
-                w +=1
-    return w
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if 0 <= nx < m and 0 <= ny < n:
+        if graph[ny][nx]:
+          q.append((ny,nx))
+          graph[ny][nx] = 0
+          tmp += 1
+  return tmp
 
+n,m = map(int, sys.stdin.readline().split())
+
+graph = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+
+result = 0
 count = 0
-big_pic = 0
-for i in range(N):
-    for j in range(M):
-        if matrix[i][j]:
-            count += 1
-            big_pic = max(bfs(i,j),big_pic)
-    
-    
+
+for i in range(n):
+  for j in range(m):
+    if graph[i][j]:
+      count += 1
+      result = max(result, bfs(i,j))
+
 print(count)
-print(big_pic)
+print(result)
